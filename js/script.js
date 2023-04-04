27006ef4a9c6f6c321292b0591dbc207093798be
hari-krishnan4274 <70365413+hari-krishnan4274@users.noreply.github.com>
2023-04-05 02:30:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
+
+for (var i = 1; i <= 20; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "images/image" + i + ".jpg";
+  thumb.alt = "Image " + i;
+  
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
+  thumb.addEventListener(
+    "click", function() {
+      currentImage.src = this.src;
+      currentImage.alt=this.alt;
+    }
+  );
+}
